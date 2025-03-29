e1b40f51f9269de8d3e1e8849c90a21aca42b3fa
claudiobentodala <programmer.claudio@gmail.com>
2025-03-29 20:19:26 +0100

    exercise about aritmetric
    using node.js

---
+++ exercise/ex008/aritmetric.js
@@ -0,0 +1,26 @@
+var n1 = 5
+var n2 = 2
+
+// + is used to sam
+var result = n1 + n2 
+`SAM: ${n1} + ${n2} = ${result}`
+
+// - is used to sub
+var sub = n1 - n2
+`SUBTRATION: ${n1} - ${n2} = ${result}`
+
+// / is used to div
+var div = n1 / n2
+`DIVISION: ${n1} / ${n2} = ${result}`
+
+// % is used to div rest
+var mult = n1 % n2
+`DIVISION REST: ${n1} % ${n2} = ${result}`
+
+// * is used to multiplicar(portuguese word)
+var mult = n1 * n2
+`MULTIPLICATION: ${n1} X ${n2} = ${result}`
+
+// ** is used to potencia(portuguese word)
+var mult = n1 ** n2
+`POTENCIA: ${n1} ^ ${n2} = ${result}`
