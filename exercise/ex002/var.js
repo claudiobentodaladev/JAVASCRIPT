ee388f7b3cdff891923158ee8f6eb53f22414c39
claudiobentodala <programmer.claudio@gmail.com>
2025-03-29 13:08:00 +0100

    second exercise
    understand var

---
+++ exercise/ex002/var.js
@@ -0,0 +1,18 @@
+/* var on javascript are not fixed 
+    you may use any datatype    */
+let anything = "Claudio"
+//or
+var any_data = 3
+
+typeof 3
+//or
+typeof anything
+
+// typeof: is used to get what datatype you are useing
+
+
+
+// let/var: you may you this two
+// any_data/anything: is identificator
+// =: is atribuition signal
+// After atribuiton: is value 
