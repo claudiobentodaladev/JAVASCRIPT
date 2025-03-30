8aa0ff2dcf3b320b1950b17241c54475272f92a3
claudiobentodala <programmer.claudio@gmail.com>
2025-03-30 17:56:19 +0100

    exercise about logic operator

---
+++ exercise/ex014/logic-operator.js
@@ -0,0 +1,15 @@
+var a = 5
+var b = 8
+
+// and: is true if the two inputs is true.
+var and
+and = true && false
+and = false && false
+and
+
+// or: is true if at least one input is true.
+var or
+or = false || false
+or
+or = true || false
+or
