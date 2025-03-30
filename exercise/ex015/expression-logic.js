c783ab0135a4507c83676db9d7bcb65d1e768d14
claudiobentodala <programmer.claudio@gmail.com>
2025-03-30 17:56:57 +0100

    exercise about logic operator
    doing some logic expression

---
+++ exercise/ex015/expression-logic.js
@@ -0,0 +1,10 @@
+var result
+//exemple01
+var years_old = 16
+result = years_old >= 13 && years_old <= 18
+result // Years old is between 13 and 18.
+
+//exemple01
+var salary = 3250,sex = 'M'
+result = salary >= 1150 &&  sex!='F'
+result //The salary is more than 1150 and is not femule
