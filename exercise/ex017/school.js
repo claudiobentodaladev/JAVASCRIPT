2b29df0629c5392df062bc1d3542e2c54d0b3b9f
claudiobentodala <programmer.claudio@gmail.com>
2025-03-30 17:58:05 +0100

    practing about operator
    doing medium system school

---
+++ exercise/ex017/school.js
@@ -0,0 +1,6 @@
+var name = 'claudio'
+var value1 = 13, value2 = 14 ,value3 = 16, medium
+
+medium = (value1 + value2 + value3) / 3
+
+medium >= 15 ? ` ${name} your medium ${medium} is nice!` : `${name} your medium is more and less, get better`
