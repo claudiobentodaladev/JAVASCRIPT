c0d5522dfff87cb7140d1ec2c78bb304308cb2bf
claudiobentodala <programmer.claudio@gmail.com>
2025-03-30 15:18:47 +0100

    exercise about operator
    relational

---
+++ exercise/ex012/relational.js
@@ -0,0 +1,20 @@
+var result
+//Relational Operator: is used to comparar(portuguese word) two value.
+
+result =  5 > 2
+result
+
+result =  7 < 4
+result
+
+result =  8 >= 8
+result
+
+result =  9 <= 7
+result
+
+result =  5 == 5
+result
+
+result =  4 != 4
+result
