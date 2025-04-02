ed0d9f4fd4957e384ac48f42058a31a53d0f27e6
claudiobentodala <programmer.claudio@gmail.com>
2025-04-02 09:57:40 +0100

    exercise about var
    putting explication about exercise

--- exercise/ex017/school.js
+++ exercise/ex017/school.js
@@ -1,6 +1,9 @@
+/*This program was created to get, 
+ whether student reprovou(portuguese word) or aprovavou(portuguese word)*/
+
 var name = 'claudio'
 var value1 = 13, value2 = 14 ,value3 = 16, medium
 
-medium = (value1 + value2 + value3) / 3
+medium = ( value1 + value2 + value3 ) / 3
 
 medium >= 15 ? ` ${name} your medium ${medium} is nice!` : `${name} your medium is more and less, get better`
