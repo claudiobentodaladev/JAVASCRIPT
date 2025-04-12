1421d8ce56ecb1a3125ab3e7c6812df3582d8334
claudiobentodala <programmer.claudio@gmail.com>
2025-04-12 15:17:16 +0100

    javascript pratics

--- exercise/ex036/script/years_olds-checker.js
+++ exercise/ex036/script/years_olds-checker.js
@@ -1,2 +1,11 @@
-var barth = window.document.querySelector('input#ibarth')
-var sexo
+function check(){
+    var time = new Date()
+    var barth = window.document.querySelector('input#ibarth')//Não esta a ficar number
+    var years = Number(barth)
+    var years_olds = time.getFullYear() - years
+    //Pegar valor do radio
+    var sex = window.document.querySelector('input#f').value
+    var show = window.document.querySelector('p#show')
+    //if..else do sexo
+    show.innerText = `${sex} ${years_olds} years old`
+}
