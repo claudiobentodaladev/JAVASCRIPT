75824d15393dbc81ad2cc84693626ce2f4d649b4
claudiobentodala <programmer.claudio@gmail.com>
2025-04-13 18:18:48 +0100

    exercise about repeticion
    about while and do...while

--- exercise/ex038/repeticion.js
+++ exercise/ex038/repeticion.js
@@ -1,5 +1,5 @@
 var years_old = 16
-// while is used to repeat a condition, while is true
+// while is used to repeat a condition, while is true (but logic test last)
 while(years_old <= 18){
-    console.log(`You can not vote, because you are 16...`)
+    console.log(`You can not vote, because you are ${years_old}...`)
 }
---
+++ exercise/ex039/repeticio-2.js
@@ -0,0 +1,5 @@
+var years_old = 16
+// while is used to repeat a condition, while is true(but logic test first)
+do{
+    console.log(`You can not vote, because you are ${years_old}...`)
+}while(years_old <= 18)
