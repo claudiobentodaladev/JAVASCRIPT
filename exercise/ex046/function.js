e82c71402b261bf04296c215b4ff8859852105bd
claudiobentodala <programmer.claudio@gmail.com>
2025-04-14 13:04:53 +0100

    deep dive about fuction

---
+++ exercise/ex046/function.js
@@ -0,0 +1,9 @@
+function even_odd(numbers){
+    if(numbers % 2 == 0) {
+        return`even number`
+    } else {
+        return`odd number`
+    }
+}
+var resulted = even_odd(12)
+console.log(resulted)
