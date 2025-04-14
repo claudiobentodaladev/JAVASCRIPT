e158c73706d7d05f981bf4a6834886250ebf0d4a
claudiobentodala <programmer.claudio@gmail.com>
2025-04-14 13:17:58 +0100

    function with two pararm

---
+++ exercise/ex047/functio-2.js
@@ -0,0 +1,7 @@
+function sam(number1 = 0,number2 = 0){
+    /*return number1 + number*/
+            //OR
+    var show = number1 + number2
+    return `${number1} + ${number2} = ${show}`
+}
+console.log(sam(4,2))
