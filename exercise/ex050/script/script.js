96883fa579eaad3589b1be5ddad712cbee1742ea
claudiobentodala <programmer.claudio@gmail.com>
2025-04-15 11:10:24 +0100

    First part of exercise

---
+++ exercise/ex050/script/script.js
@@ -0,0 +1,11 @@
+var number = window.document.querySelector('input#inum')
+var inputs = window.document.querySelector('p#inputs')
+var length = window.document.querySelector('p#resulted')
+var valores = []
+function add(){
+    inputs = ''
+    inputs.innerHTML += Number(number.value)
+}
+function show(){
+    
+}
