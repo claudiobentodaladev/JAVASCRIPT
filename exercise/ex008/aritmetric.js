cc8858c530cc97c74c3f1ddf9f817a133015e617
claudiobentodala <programmer.claudio@gmail.com>
2025-04-17 13:42:42 +0100

    put console.log on aritmetric exercise

--- exercise/ex008/aritmetric.js
+++ exercise/ex008/aritmetric.js
@@ -1,26 +1,23 @@
 var n1 = 5
 var n2 = 2
+var result
 
 // + is used to sam
 var result = n1 + n2 
-`SAM: ${n1} + ${n2} = ${result}`
-
-// - is used to sub
+console.log(`SAM: ${n1} + ${n2} = ${result}`)
+// - is used to subtracion
 var sub = n1 - n2
-`SUBTRATION: ${n1} - ${n2} = ${result}`
-
-// / is used to div
+console.log(`SUBTRATION: ${n1} - ${n2} = ${result}`)
+// / is used to division
 var div = n1 / n2
-`DIVISION: ${n1} / ${n2} = ${result}`
-
+console.log(`DIVISION: ${n1} / ${n2} = ${result}`)
 // % is used to div rest
 var mult = n1 % n2
-`DIVISION REST: ${n1} % ${n2} = ${result}`
+console.log(`DIVISION REST: ${n1} % ${n2} = ${result}`)
 
-// * is used to multiplicar(portuguese word)
+// * is used to multiplication
 var mult = n1 * n2
-`MULTIPLICATION: ${n1} X ${n2} = ${result}`
-
+console.log(`MULTIPLICATION: ${n1} X ${n2} = ${result}`)
 // ** is used to potencia(portuguese word)
 var mult = n1 ** n2
-`POTENCIA: ${n1} ^ ${n2} = ${result}`
+console.log(`POTENCIA: ${n1} ^ ${n2} = ${result}`)
