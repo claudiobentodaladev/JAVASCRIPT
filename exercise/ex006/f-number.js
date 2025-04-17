5879b11c5d0b8caf61fd29b5c4e011d0dc748e41
claudiobentodala <programmer.claudio@gmail.com>
2025-04-17 13:38:17 +0100

    put console.log on format text

--- exercise/ex006/f-number.js
+++ exercise/ex006/f-number.js
@@ -2,6 +2,6 @@ var number = 3.1440
 
 number.toFixed(2)
 //toFixed: is used to allow how many number you wont to after point
-`${number}`
+console.log(`${number}`)
 
 number.toFixed(2).replace('.',',')
