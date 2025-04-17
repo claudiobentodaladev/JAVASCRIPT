bfa2260537a72dee120203327571e88c90c7276f
claudiobentodala <programmer.claudio@gmail.com>
2025-04-17 14:04:28 +0100

    put console.log on exercise about logic expression

--- exercise/ex015/expression-logic.js
+++ exercise/ex015/expression-logic.js
@@ -2,9 +2,9 @@ var result
 //exemple01
 var years_old = 16
 result = years_old >= 13 && years_old <= 18
-result // Years old is between 13 and 18.
+console.log(result) // Years old is between 13 and 18.
 
 //exemple01
 var salary = 3250,sex = 'M'
 result = salary >= 1150 &&  sex!='F'
-result //The salary is more than 1150 and is not femule
+console.log(result) //The salary is more than 1150 and is not femule
