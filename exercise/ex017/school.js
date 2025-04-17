cf7fca733f9f462ceacc7c0f614a036dfd4a8244
claudiobentodala <programmer.claudio@gmail.com>
2025-04-17 14:06:19 +0100

    put console.log on practice about school

--- exercise/ex017/school.js
+++ exercise/ex017/school.js
@@ -6,4 +6,4 @@ var value1 = 13, value2 = 14 ,value3 = 16, medium
 
 medium = ( value1 + value2 + value3 ) / 3
 
-medium >= 15 ? ` ${name} your medium ${medium} is nice!` : `${name} your medium is more and less, get better`
+medium >= 15 ?console.log(` ${name} your medium ${medium} is nice!`) :console.log(`${name} your medium is more and less, get better`)
