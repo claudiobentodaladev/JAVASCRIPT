2f3506c7498c8554426052165957529f4e408d58
claudiobentodala <programmer.claudio@gmail.com>
2025-04-17 14:01:06 +0100

    put console.log on exercise about relational operator

--- exercise/ex012/relational.js
+++ exercise/ex012/relational.js
@@ -2,19 +2,19 @@ var result
 //Relational Operator: is used to comparar(portuguese word) two value.
 
 result =  5 > 2
-result
+console.log(result)
 
 result =  7 < 4
-result
+console.log(result)
 
 result =  8 >= 8
-result
+console.log(result)
 
 result =  9 <= 7
-result
+console.log(result)
 
 result =  5 == 5
-result
+console.log(result)
 
 result =  4 != 4
-result
+console.log(result)
