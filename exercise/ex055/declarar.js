db2211bbd30392e3097febe2a22da658e4782136
claudiobentodala <programmer.claudio@gmail.com>
2025-04-18 16:36:53 +0100

    changed to portuguse word declaration

--- exercise/ex055/declarar.js
+++ exercise/ex055/declarar.js
@@ -1,4 +1,4 @@
-//Declaração: is used to separed the code, no go to another line 
+//Declaration: is used to separed the code, no go to another line 
 var a,b,c; a = 1; b = 2; c = 3;console.log(` ${a} + ${b} + ${c} = ${a+b+c}`);console.log('hello')
 /*  OR  
 var a,b,c
