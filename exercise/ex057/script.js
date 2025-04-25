0e963ea5fb13804c5ab2bdc5e1c100d8d26f567f
claudiobentodala <programmer.claudio@gmail.com>
2025-04-25 13:15:39 +0100

    complete exercise about load

--- exercise/ex057/script.js
+++ exercise/ex057/script.js
@@ -7,15 +7,16 @@ function loaded(){
 let head = window.document.querySelector('header')
 let main = window.document.querySelector('main')
 let foot = window.document.querySelector('footer')
+let bod = window.document.querySelector('div#body')
 if(load()) {
     head.style.display = 'none'
     main.style.display = 'none'
     foot.style.display = 'none'
-    alert('wait...')
+    bod.style.display = 'flex'
 }
 if(loaded()){
+    bod.style.display = 'none'
     head.style.display = 'block'
     main.style.display = 'block'
     foot.style.display = 'block'
-    alert('ok')
 }
