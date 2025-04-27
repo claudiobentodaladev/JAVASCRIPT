b27089e170f01e2f909b8ca2caebccd694384b83
claudiobentodala <programmer.claudio@gmail.com>
2025-04-27 12:22:22 +0100

    Did a litle about exercise logic

---
+++ exercise/ex061/go.js
@@ -0,0 +1,14 @@
+function go() {
+    var name = window.document.querySelector('input#iname').value
+    var language = window.document.querySelector('select#ilang').value
+    var show = window.document.querySelector('p#show')
+
+    if (name == "") {
+        window.alert('IPUT NAME!')
+    } else{
+        show.innerHTML =`${name} speak ${language}`
+    }
+    window.open('show.html')
+
+
+}
