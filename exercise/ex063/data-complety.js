d1aabbfe7716597e3ece5d2a7bbbcd7f08fd8af6
claudiobentodala <programmer.claudio@gmail.com>
2025-05-03 11:30:57 +0100

    DID YHE EXERCISE ABOUT COMPLETY DATE

---
+++ exercise/ex063/data-complety.js
@@ -0,0 +1,10 @@
+//complety date
+let date = new Date()
+let months
+let day
+let years = date.getFullYear()
+if (date.getMonth() >= 0 && date.getMonth()<= 9) {months = `0${date.getMonth()}`}
+else{months = date.getMonth()}
+if (date.getDate() >= 0 && date.getDate()<= 9) {day = `0${date.getDate()}`}
+else{day = date.getMonth()}
+console.log(`${day}/${months}/${years}`)
