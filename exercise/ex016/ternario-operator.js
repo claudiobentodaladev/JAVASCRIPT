fa3b771b993703bf52d4767f6511fa5c6ef370c4
claudiobentodala <programmer.claudio@gmail.com>
2025-05-03 11:57:05 +0100

    DID EXEICISE ABOUT EXPIRATIONS

--- exercise/ex016/ternario-operator.js
+++ exercise/ex016/ternario-operator.js
@@ -4,5 +4,5 @@ var medium = 8.5
 medium >= 10 ? 'Apto' : 'Reprovado'
 
 var x = 5
-x += x  > 10?5:1
+x += x  > 10 ? 5 :1
 console.log(x)
---
+++ exercise/ex064/date-exp.js
@@ -0,0 +1,5 @@
+let today = new Date();
+let start = new Date(2025,4,16);
+let exp = new Date(2025,8,30);
+
+today < start && today < exp? 'you may' : 'you maynot'
