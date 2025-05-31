3f7d0515199f467827df67befba91009a7b61f9a
claudiobentodala <programmer.claudio@gmail.com>
2025-05-31 17:42:29 +0100

    FINISHED EXERCISE ABOUT ARROW FUNCTION

--- exercise/ex066/arrow-function.js
+++ exercise/ex066/arrow-function.js
@@ -1,5 +1,18 @@
+const n1 = 10,n2 = 5;
+
 function subtracion(a,b) {
     return a - b;
 }
-let n1 = 3 
-let n2 = 2
+console.log(`NORMAL FUNCTION: ${subtracion(n1,n2)}`);
+
+/*  Arrow function is diferent way to creat function in javascript
+
+const subtracion_ = (a,b) => {
+    return a - b;
+}
+*/
+
+//SHORT FORM, but only is used when the function only have the return line.
+const subtracion_ = (a,b) =>  a - b;
+
+console.log(`ARROW FUNCTION: ${subtracion_(n1,n2)}`)
