4414e15a35e2a068aad31b4759e7306152756d7d
claudiobentodala <programmer.claudio@gmail.com>
2025-06-06 09:45:46 +0100

    EMAIL REGEX EXPRESSION EXERCISE

---
+++ exercise/ex071/regex-email.js
@@ -0,0 +1,8 @@
+let email = 'programmer.claudio@gmail.com';
+let regex_email = /([a-z0-9\.\-]{2,})\@([a-z0-9]{2,})([\.a-z0-9]{2,})([\.a-z0-9]{2,})?/g ;
+
+let email_checker_text = email.match(regex_email)
+let email_checker_bool = regex_email.test(email)
+
+console.log(email_checker_text)
+console.log(email_checker_bool)
