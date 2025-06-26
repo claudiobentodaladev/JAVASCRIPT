64873d94abb81ffec4ef15e4e23d070fcfc746b4
claudiobentodala <programmer.claudio@gmail.com>
2025-06-26 12:10:21 +0100

    remenber about continue

--- exercise/ex074/continue.js
+++ exercise/ex074/continue.js
@@ -1,8 +1,8 @@
 // continue is used to skip a instrucion
 for (let i = 0; i < 10; i++){
+    i++;console.log(`${i}`);i--
     if (i == 6) {
         console.log(`skiped in ${i}`)
-        continue;
+        continue; // the instrucion is skiped in this and then continued
     }
-    i++;console.log(`${i}`);i--
 }
