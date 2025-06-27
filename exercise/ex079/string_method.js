694053de197427ac57fa15e0fa136486376395a7
claudiobentodala <programmer.claudio@gmail.com>
2025-06-27 13:23:09 +0100

    String method: learning how it work

---
+++ exercise/ex079/string_method.js
@@ -0,0 +1,16 @@
+var text = 'claudio'
+console.log(`Length: ${text.length}`) // length used to get how many char we got
+
+// STRING METHOD
+
+console.log(`Charat 2 : ${text.charAt(2)}`) // charAt is used to get where is char acording to your position
+
+console.log(`Includes: ${text.includes('claj')}`) // includes is used to get if a string is in other string
+
+console.log(`Repeat: ${text.repeat(3)}`) // repeat is used to repeat a string
+
+console.log(`Slice: ${text.slice(2,5)}`) // slice is used to get a part of string
+
+console.log(`uppercase: ${text.toUpperCase()}`)
+
+console.log(`lowercase: ${'CLAUDIO'.toLowerCase()}`)
