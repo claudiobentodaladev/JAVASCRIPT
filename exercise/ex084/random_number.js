c760321fe308d817b1bda18d746585602cb1aee6
claudiobentodala <programmer.claudio@gmail.com>
2025-06-28 12:52:15 +0100

    math random: got better comentary

--- exercise/ex084/random_number.js
+++ exercise/ex084/random_number.js
@@ -1,4 +1,4 @@
-// This script is to get a random number
+// This script is to get a random number between two number
 
 var max = 1000, min = 100
 var random = Math.trunc(Math.random() * (max -min)) + min
