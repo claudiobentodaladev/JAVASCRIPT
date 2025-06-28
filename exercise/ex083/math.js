63651e14977d569c88aeaad1152e4f75dca9c5a0
claudiobentodala <programmer.claudio@gmail.com>
2025-06-28 12:43:44 +0100

    math: learning about math method

---
+++ exercise/ex083/math.js
@@ -0,0 +1,23 @@
+// MATH
+
+console.log(`PI: ${Math.PI}`) // Math.PI is used to get PI value
+
+// methods
+
+console.log(`absolute: ${Math.abs(-14)}`) // Math.abs() is used to show always positive numbers
+
+console.log(`Square root of 2 is ${Math.sqrt(2)}`) // Math.sqrt is used to get « raiz quadrada »
+
+console.log(`CBRT of 100 is ${Math.cbrt(100)}`) // Math.cbrt() is used to get « raiz cubica »
+
+console.log(`CEIL of 3.2 is ${Math.ceil(3.2)}`) // Math.ceil() is used to « arrendodar por excesso »
+
+console.log(`FLOOR of 3.2 is ${Math.floor(3.2)}`) // Math.floor() is used to « arrendodar por defeito »
+
+console.log(`ROUND of 3.4 is ${Math.round(3.4)}`) // Math.round() is used to « arrendodar de forma dinâmica »
+
+console.log(`TRUNC: ${Math.trunc(3.2)}`) // Math.trunc() is used to remove « casas decimais »
+
+console.log(`POTENCE: ${Math.pow(3,2)}`) // Math.pow(base,exp) is used to « calcular potência »
+
+console.log(`RANDOM: ${Math.random()}`) // Math.random() is used to get a random number
