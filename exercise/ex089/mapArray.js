621175be604e5773b95b5e172533ef1516e4c105
claudiobentodaladev <programmer.claudio@gmail.com>
2025-07-29 12:46:25 +0100

    ARRAY: study about map.

---
+++ exercise/ex089/mapArray.js
@@ -0,0 +1,11 @@
+const names = ['claudio', 'bento', 'dala']
+/*
+for (const index in names) {
+   console.log(names[index])
+}
+*/
+
+// map is a native function to make the same than forIn.
+names.map( index =>{
+    console.log(index)
+})
