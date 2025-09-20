635efaf599955e8556869c3b8767e50c12128c76
claudiobentodaladev <programmer.claudio@gmail.com>
2025-09-20 10:07:16 +0100

    MODULE: formated

--- exercise/ex068 - module/module-main.js
+++ exercise/ex068 - module/module-main.js
@@ -1,8 +1,8 @@
-const {add,greeting,subtract} = require('./module.child') // require is used to import function and data from other file 
+const { add, greeting, subtract } = require('./module.child') // require is used to import function and data from other file 
 
 console.log("parent")
 
-console.log(`add ${add(1,3)}`)
-console.log(`subtract ${subtract(1,3)}`)
+console.log(`add ${add(1, 3)}`)
+console.log(`subtract ${subtract(1, 3)}`)
 
 console.log(`Data in other file: ${greeting}`)
