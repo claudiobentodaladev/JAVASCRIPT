eff27885ce6d4fc2e82f1e4aa8d7d7bd24bca990
claudiobentodaladev <programmer.claudio@gmail.com>
2025-11-21 10:01:19 +0100

    INCLUDES: stydy how it works

---
+++ exercise/ex096-includes/includes.js
@@ -0,0 +1,7 @@
+// includes is used to check if a value is aqual to any in value in an array
+
+const any_value = "claudio"
+
+const included = ["claudio","bento","dala"].includes(any_value)
+
+console.log(included)
