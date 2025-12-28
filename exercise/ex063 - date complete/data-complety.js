ab7f02f58e82996949abfa7301d7bfa92683540c
claudiobentodaladev <programmer.claudio@gmail.com>
2025-12-28 15:58:25 +0100

    DATE: organized the date

--- exercise/ex063 - date complete/data-complety.js
+++ exercise/ex063 - date complete/data-complety.js
@@ -7,4 +7,5 @@ if (date.getMonth() >= 0 && date.getMonth()<= 9) {months = `0${date.getMonth()}`
 else{months = date.getMonth()}
 if (date.getDate() >= 0 && date.getDate()<= 9) {day = `0${date.getDate()}`}
 else{day = date.getMonth()}
-console.log(`${day}/${months}/${years}`)
+console.log(`/${years}/${months}/${day}`)
+console.log(date)
