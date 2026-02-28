27c2a8bc3e4079f297c60d7108bc45bf0c52d85b
claudiobentodaladev <programmer.claudio@gmail.com>
2026-02-28 10:11:21 +0100

    organized

--- exercise/ex105-fullDate/fullDate.js
+++ exercise/ex105-fullDate/fullDate.js
@@ -1,13 +1,13 @@
-var months = ['jan','feb','mar','apri','may','jun','jul','ago','set','oct','nov','dec']
+var months = ['jan', 'feb', 'mar', 'apri', 'may', 'jun', 'jul', 'ago', 'set', 'oct', 'nov', 'dec']
 
 const date = {
-    year:  new Date().getFullYear(),
+    year: new Date().getFullYear(),
     month: months[new Date().getMonth()],
     day: new Date().getDate(),
     hour: new Date().getHours(),
     minute: new Date().getMinutes(),
     seconds: new Date().getSeconds(),
-    milliseconds:  new Date().getMilliseconds()
+    milliseconds: new Date().getMilliseconds()
 };
 
 const fullDate = `${date.month} ${date.day} ${date.year} ${date.hour}:${date.minute}:${date.seconds}:${date.milliseconds}`
