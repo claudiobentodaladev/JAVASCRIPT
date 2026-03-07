d8c140da6685ba92eca4558699e01d469cb03604
claudiobentodaladev <programmer.claudio@gmail.com>
2026-03-07 11:57:37 +0100

    REGEX

--- exercise/ex071-regEx_email/regex-email.js
+++ exercise/ex071-regEx_email/regex-email.js
@@ -1,8 +1,22 @@
-let email = 'programmer.claudio@gmail.com';
-let regex_email = /([a-z0-9\.\-]{2,})\@([a-z0-9]{2,})\.([\.a-z0-9]{2,})([\.a-z0-9]{2,})?/gi ;
+const readline = require("readline");
 
-let email_checker_text = email.match(regex_email)
-let email_checker_bool = regex_email.test(email)
+console.log("input email to check: ")
 
-console.log(email_checker_text)
-console.log(email_checker_bool)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let regex_email = /([a-z0-9\.\-]{2,})\@([a-z0-9]{2,})\.([\.a-z0-9]{2,})([\.a-z0-9]{2,})?/gi;
+rl.question("", (email) => {
+
+    let email_checker_text = email.match(regex_email)
+    let email_checker_bool = regex_email.test(email)
+
+    if (email_checker_bool) console.log("VALIDE EMAIL!")
+    else console.log("NOT VALIDE EMAIL!")
+
+    console.log(email_checker_text)
+    console.log(email_checker_bool)
+    rl.close();
+});
